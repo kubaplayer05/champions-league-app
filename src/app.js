e84518dba0509204571c0652cc3e527883f5cfa1
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2022-10-15 16:56:24 +0200

    fetch data is working
    add createGroupTeam function in utils.js and export it, import function to app.js, remove placeholders from group-list

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+import { crateGroupTeam } from './utils.js'
+
 const API_KEY = '7d1c90cee2546c046d41ff171199d7ee'
 const currentSeason = 2022
+
+const groupList = document.querySelectorAll('.group-list')
 
 const myHeaders = new Headers()
 myHeaders.append('x-rapidapi-key', API_KEY)
@@ -16,5 +20,29 @@ fetch(`https://v3.football.api-sports.io/standings?league=2&season=${currentSeas
 	.then(result => {
 		const standings = result.response[0].league.standings
 		console.log(standings)
+
+		groupList.forEach(list => {
+			console.log(list)
+			const index = list.attributes[1].value
+
+			const result = standings[index].map(team => {
+				return {
+					rank: team.rank,
+					name: team.team.name,
+					gamesPlayed: team.all.played,
+					wins: team.all.win,
+					draws: team.all.draw,
+					losses: team.all.lose,
+					goalDiff: team.goalsDiff,
+					points: team.points,
+				}
+			})
+
+			result.forEach(el => {
+				list.append(crateGroupTeam(el))
+			})
+		})
 	})
 	.catch(error => console.log('error', error))
+
+console.log(groupList)
---
+++ src/utils.js
@@ -0,0 +1,42 @@
+export { crateGroupTeam }
+
+const crateGroupTeam = team => {
+	const li = document.createElement('li')
+	const leftSide = document.createElement('div')
+	const rightSide = document.createElement('div')
+	const rank = document.createElement('span')
+	const name = document.createElement('p')
+	const gamesPlayed = document.createElement('span')
+	const wins = document.createElement('span')
+	const draws = document.createElement('span')
+	const losses = document.createElement('span')
+	const goalDiff = document.createElement('span')
+	const points = document.createElement('span')
+
+	li.classList.add('group-team')
+	leftSide.classList.add('left-side')
+	rightSide.classList.add('right-side')
+	rank.classList.add('rank')
+	name.classList.add('team-name')
+	gamesPlayed.classList.add('games-played')
+	wins.classList.add('wins')
+	draws.classList.add('draws')
+	losses.classList.add('losses')
+	goalDiff.classList.add('goal-diff')
+	points.classList.add('points')
+
+	rank.textContent = team.rank
+	name.textContent = team.name
+	gamesPlayed.textContent = team.gamesPlayed
+	wins.textContent = team.wins
+	draws.textContent = team.draws
+	losses.textContent = team.losses
+	goalDiff.textContent = team.goalDiff
+	points.textContent = team.points
+
+	leftSide.append(rank, name)
+	rightSide.append(gamesPlayed, wins, draws, losses, goalDiff, points)
+	li.append(leftSide, rightSide)
+
+	return li
+}
