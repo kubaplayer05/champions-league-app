94761b67408036f8655871149d72e796a492f6b9
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2022-10-30 12:54:20 +0100

    add statistics.html
    add comments in app.js, a little changes in css for mobile navigation, add section validation and create proper functions for loading data in app.js,

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import { crateGroupTeam } from './utils.js'
 
+// set fetch configuration
+
 const API_KEY = '7d1c90cee2546c046d41ff171199d7ee'
 const currentSeason = 2022
-
-const groupList = document.querySelectorAll('.group-list')
 
 const myHeaders = new Headers()
 myHeaders.append('x-rapidapi-key', API_KEY)
@@ -15,38 +15,56 @@ const requestOptions = {
 	redirect: 'follow',
 }
 
-fetch(`https://v3.football.api-sports.io/standings?league=2&season=${currentSeason}`, requestOptions)
-	.then(response => response.json())
-	.then(result => {
-		const standings = result.response[0].league.standings
-		console.log(standings)
+// end of configuration
 
-		groupList.forEach(list => {
-			const index = list.attributes[1].value
-			const result = standings[index].map(team => {
-				return {
-					rank: team.rank,
-					name: team.team.name,
-					gamesPlayed: team.all.played,
-					wins: team.all.win,
-					draws: team.all.draw,
-					losses: team.all.lose,
-					goalDiff: team.goalsDiff,
-					points: team.points,
-					logo: team.team.logo,
-				}
-			})
+// functions section
 
-			result.forEach(el => {
-				list.append(crateGroupTeam(el))
+const loadStandings = () => {
+	fetch(`https://v3.football.api-sports.io/standings?league=2&season=${currentSeason}`, requestOptions)
+		.then(response => response.json())
+		.then(result => {
+			// get all lists that are in container
+			const groupList = document.querySelectorAll('.group-list')
+			// get array with arrays that represent groups
+			const standings = result.response[0].league.standings
+			console.log(standings)
+
+			// get data and render it for each list
+			groupList.forEach(list => {
+				const index = list.attributes[1].value
+				const result = standings[index].map(team => {
+					return {
+						rank: team.rank,
+						name: team.team.name,
+						gamesPlayed: team.all.played,
+						wins: team.all.win,
+						draws: team.all.draw,
+						losses: team.all.lose,
+						goalDiff: team.goalsDiff,
+						points: team.points,
+						logo: team.team.logo,
+					}
+				})
+
+				result.forEach(el => {
+					list.append(crateGroupTeam(el))
+				})
 			})
 		})
-	})
-	.catch(error => console.log('error', error))
+		.catch(error => console.log('error', error))
+}
+
+const loadStatistics = () => {}
+
+// end of functions section
+
+// This is mobile section
 
 const menuIcon = document.querySelector('.hamburger-menu')
 const closeIcon = document.querySelector('.close-icon')
 const mobileNavigation = document.querySelector('.mobile-navigation')
+
+// add events on icons
 
 menuIcon.addEventListener('click', () => {
 	mobileNavigation.classList.remove('rotate')
@@ -55,3 +73,17 @@ menuIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
 	mobileNavigation.classList.add('rotate')
 })
+
+// end of mobile section
+
+// get current localization on app and load proper function
+
+const section = window.location.pathname
+
+switch (section) {
+	case '/index.html':
+		loadStandings()
+		break
+	case '/statistics.html':
+		break
+}
